061b96621b7b6bf1d7a1f1e6e8998d47ac3ab878
tomaza <tomaza8402@gmail.com>
2022-06-07 00:17:57 +0100

    Add files via upload

---
+++ BoswanAuthApi/InfoAssetsLoader.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using SeamlessHr.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeamlessHr.Service.Services
+{
+    public class InfoAssetsLoader
+    {
+        public static string LoadAssets()
+        {
+            return ScriptLoader.GetString("InfoAssets.txt");
+        }
+        public static string LoadAssets2()
+        {
+            return ScriptLoader.GetString("InfoAssets2.txt");
+        }
+
+        public static InfoAsset GetAsset()
+        {
+            var assetString = LoadAssets();
+            var assetObj = JsonConvert.DeserializeObject<InfoAssetObj>(assetString);
+            return assetObj.Asset;
+        }
+
+        public static InfoAsset GetAsset2()
+        {
+            var assetString = LoadAssets2();
+            var assetObj = JsonConvert.DeserializeObject<InfoAssetObj>(assetString);
+            return assetObj.Asset;
+        }
+
+
+    }
+}
---
+++ BoswanAuthApi/ScriptLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeamlessHr.Service.Services
+{
+    public class ScriptLoader
+    {
+        public static string GetString(string name, Assembly assembly)
+        {
+            string data = null;
+            var res = assembly.GetManifestResourceNames();
+            string resName = res.FirstOrDefault(mf => mf.EndsWith(name));
+            if (resName != null)
+            {
+                using (StreamReader sr = new StreamReader(assembly.GetManifestResourceStream(resName)))
+                {
+                    data = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+
+            return data;
+        }
+        public static string GetString(string name)
+        {
+            return ScriptLoader.GetString(name, typeof(ScriptLoader).Assembly);
+        }
+    }
+}
